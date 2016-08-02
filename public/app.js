046657c248dd258458b13044c1e387ffea643295
Paddywalsh89 <paddy@planet9energy.com>
2016-08-02 18:49:30 +0100

    Added Navbar and Skip buttons to page. Also added counter to end iteration at end of problem list

--- public/app.js
+++ public/app.js
@@ -1,35 +1,117 @@
-// 'use strict';
-// var learnjs = {};
-// learnjs.showView = function(hash){
-// 	var problemView = $('<div class="problem-view">').text('Coming soon!');
-// $('.view-container').empty().append(problemView);	
-// }
+/***
+ * Excerpted from "Serverless Single Page Apps",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/brapps for more book information.
+***/
+"use strict";
 
+var learnjs = {};
 
-'use strict';
-var learnjs={};
-learnjs.problemView = function(problemNumber) {
-	var view = $('.templates .problem-view').clone();
-	view.find('.title').text('Problem #' + problemNumber);
-	return view;
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem() { return 42 === 6 * __; }"
+  }
+];
+
+learnjs.triggerEvent = function(name, args) {
+  $('.view-container>*').trigger(name, args);
 }
 
-learnjs.showView=function(hash){
-	var routes = {
-		'#problem': learnjs.problemView
-	};
-	var hashParts = hash.split('-');
-	var viewFn = routes[hashParts[0]];
-	if (viewFn) {
-		 $('.view-container').empty().append(viewFn(hashParts[1])); //does this mean that that value of hash has been appended to 42. 
-	}
+learnjs.template = function(name) {
+  return $('.templates .' + name).clone();
 }
 
-learnjs.appOnReady = function(){
-	window.onhashchange = function(){
-		learnjs.showView(window.location.hash);
-	};
-	learnjs.showView(window.location.hash);
-}
-//I don't really understand this
+learnjs.applyObject = function(obj, elem) {
+  for (var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+};
 
+learnjs.flashElement = function(elem, content) {
+  elem.fadeOut('fast', function() {
+    elem.html(content);
+    elem.fadeIn();
+  });
+}
+
+learnjs.buildCorrectFlash = function (problemNum) {
+  var correctFlash = learnjs.template('correct-flash');
+  var link = correctFlash.find('a');
+  if (problemNum < learnjs.problems.length) {
+    link.attr('href', '#problem-' + (problemNum + 1));
+  } else {
+    link.attr('href', '');
+    link.text("You're Finished!");
+  }
+  return correctFlash;
+}
+
+learnjs.problemView = function(data) {
+  var problemNumber = parseInt(data, 10);
+  var view = learnjs.template('problem-view');
+  var problemData = learnjs.problems[problemNumber - 1];
+  var resultFlash = view.find('.result');
+
+  function checkAnswer() {
+    var answer = view.find('.answer').val();
+    var test = problemData.code.replace('__', answer) + '; problem();';
+    return eval(test);
+  }
+
+  function checkAnswerClick() {
+    if (checkAnswer()) {
+      var flashContent = learnjs.buildCorrectFlash(problemNumber);
+      learnjs.flashElement(resultFlash, flashContent);
+    } else {
+      learnjs.flashElement(resultFlash, 'Incorrect!');
+    }
+    return false;
+  }
+
+  if (problemNumber < learnjs.problems.length) {
+    var buttonItem = learnjs.template('skip-btn');
+    buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+    $('.nav-list').append(buttonItem);
+    view.bind('removingView', function() {
+      buttonItem.remove();
+    });
+  }
+
+  view.find('.check-btn').click(checkAnswerClick);
+  view.find('.title').text('Problem #' + problemNumber);
+  learnjs.applyObject(problemData, view);
+  return view;
+}
+
+learnjs.landingView = function() {
+  return learnjs.template('landing-view');
+}
+
+learnjs.showView = function(hash) {
+  var routes = {
+    '#problem': learnjs.problemView,
+    '#': learnjs.landingView,
+    '': learnjs.landingView
+  };
+  var hashParts = hash.split('-');
+  var viewFn = routes[hashParts[0]];
+  if (viewFn) {
+    learnjs.triggerEvent('removingView', []);
+    $('.view-container').empty().append(viewFn(hashParts[1]));
+  }
+}
+
+learnjs.appOnReady = function() {
+  window.onhashchange = function() {
+    learnjs.showView(window.location.hash);
+  };
+  learnjs.showView(window.location.hash);
+}
